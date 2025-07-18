7f1078c2065013ec51f8abcbac8312584df37773
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-18 12:46:14 -0300

    Create db.py

---
+++ db.py
@@ -0,0 +1,10 @@
+#Conexão com MySQL
+import mysql.connector
+
+def conectar():
+    return mysql.connector.connect(
+        host = "localhost",
+        user = "seu_usuario_mysql",
+        password = "sua_senha_mysql",
+        database = "sistema_login"
+    )
