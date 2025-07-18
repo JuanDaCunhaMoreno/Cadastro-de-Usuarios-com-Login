7e726e602bb03499b8a586f1e92079394874255d
Juan Moreno <juandacunhamoreno@gmail.com>
2025-07-18 13:36:10 -0300

    Update db.py

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ import mysql.connector
 
 def conectar():
     return mysql.connector.connect(
-        host = "localhost",
-        user = "seu_usuario_mysql",
-        password = "sua_senha_mysql",
-        database = "sistema_login"
+        host= "localhost",
+        user= "seu_usuario_mysql",
+        password= "sua_senha_mysql",
+        database= "sistema_login"
     )
